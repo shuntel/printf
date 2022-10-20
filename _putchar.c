6ffab8acabe41348feed57838810f5d7d000b5cd
shuntel <111053515+shuntel@users.noreply.github.com>
2022-10-20 16:12:53 +0100

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,9 @@
 #include <unistd.h>
+#include "main.h"
+
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: the character to print and printed
  * Return: 0 on success and -1 error and errno is set appropiately
  */
 int _putchar(char c)
